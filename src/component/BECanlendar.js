7cf35ffde234188609d1279a009ca91e82590c54
caoleyun <244814637@qq.com>
2019-05-03 21:49:16 +0800

    日期组件-5/3/3-

--- src/component/BECanlendar.js
+++ src/component/BECanlendar.js
@@ -10,13 +10,14 @@ class BECanlendar  extends React.Component{
 	constructor(){
 		super();
 
+		var d=new Date();
 		//b表示开始日期
 		//e表示结束日期
 		this.state={
 			"b":{
-				"year":2019,
-				"month":5,
-				"day":3,
+				"year":d.getFullYear(),
+				"month":d.getMonth()+1,
+				"day":d.getDate(),
 				"showCanlendar":false
 			},
 			"e":{
@@ -24,8 +25,11 @@ class BECanlendar  extends React.Component{
 				"month":10,
 				"day":1,
 				"showCanlendar":false
-			}
+			},
+			showchoosebox:false
 		}
+		//取消时候的 备份数据
+		
 	}
 
 	//此组件的显示与否
@@ -50,6 +54,69 @@ class BECanlendar  extends React.Component{
 	onpick2({year,month,day}){
 		this.setState({e:{showCanlendar:false,year,month,day}});
 	}
+
+	//计算日期间隔
+	calcGap(){
+		var dateb=new Date(this.state.b.year,this.state.b.month,this.state.b.day);
+		var datee=new Date(this.state.e.year,this.state.e.month,this.state.e.day);
+		//时间戳差
+		var gap = datee-dateb;
+
+		//换算成天
+		var gmonth = gap/1000/60/60/24;
+
+		return  gmonth;
+		
+		// //年分差
+		// var gyear = this.state.e.year-this.state.b.year;
+
+		// //
+
+	}
+
+
+	showchoosebox(){
+			return ( 
+				<div className="chooseBox">
+					<input type="button" value="确定" className="submitbtn" />
+					<input type="button" value="取消" className="cancelbtn" />
+					<div className="begin">
+						开始日期：
+						<div className="begin_result result" >
+							<div onClick={(event)=>{  /*此处事件涉及事件冒泡  会覆盖*/
+								this.setState({ 
+									b:{...this.state.b,showCanlendar:!(this.state.b.showCanlendar)},
+									e:{...this.state.e,showCanlendar:false} 
+								}); 
+							}}>
+								{this.state.b.year}年{this.state.b.month}月{this.state.b.day}日
+								<span className="glyphicon glyphicon-calendar canlendarbtn" ></span>
+							</div>
+							{this.showCanlendar1()}
+						</div>
+					</div>
+					<div className="days">
+						共<span className="month">{this.calcGap()}</span>天
+					</div>
+					<div className="end">
+						结束日期：
+						<div className="begin_result result">
+							<div onClick={(event)=>{
+								this.setState({ 
+									e:{...this.state.e,showCanlendar:!(this.state.e.showCanlendar)},
+									b:{...this.state.b,showCanlendar:false}
+								});  
+						}}>
+								{this.state.e.year}年{this.state.e.month}月{this.state.e.day}日
+								<span className="glyphicon glyphicon-calendar canlendarbtn" ></span>
+							</div>
+							{this.showCanlendar2()}
+						</div>
+					</div>
+				</div>
+			)
+	}
+
 		
 	render(){
 		// console.log(this.state.b.year,this.state.b.month,this.state.b.day);
@@ -57,32 +124,16 @@ class BECanlendar  extends React.Component{
 		
 		return (
 			<div className="BECanlendar">
-				<div className="result">
+				<div className="result" onClick={(event)=>{  /*此处事件涉及事件冒泡  会覆盖*/
+						this.setState({ 
+							showchoosebox:!this.state.showchoosebox
+						}); 
+				}}>
 					2016年6月7日 - 2019年4月30日
 					<span className="glyphicon glyphicon-calendar canlendarbtn"></span>
 				</div>
-				<div className="chooseBox">
-					<div className="begin">
-						开始日期：
-						<div className="begin_result result" onClick={(event)=>{  /*此处事件涉及事件冒泡  会覆盖*/if(event.currentTarget==event.target){ if(this.state.b.showCanlendar){ this.setState({b:{...this.state.b,showCanlendar:false}}); }else{ this.setState({b:{...this.state.b,showCanlendar:true}});}  }}}>
-							{this.state.b.year}年{this.state.b.month}月{this.state.b.day}日
-							<span className="glyphicon glyphicon-calendar canlendarbtn" ></span>
-							{this.showCanlendar1()}
-						</div>
-					</div>
-					<div className="days">
-						共<span className="month">6</span>月
-					</div>
-					<div className="end">
-						结束日期：
-						<div className="begin_result result" onClick={(event)=>{if(event.currentTarget==event.target){  if(this.state.e.showCanlendar){ this.setState({e:{...this.state.e,showCanlendar:false}}); }else{ this.setState({e:{...this.state.e,showCanlendar:true}});} } }}>
-							{this.state.e.year}年{this.state.e.month}月{this.state.e.day}日
-							<span className="glyphicon glyphicon-calendar canlendarbtn" ></span>
-							{this.showCanlendar2()}
-						</div>
-					</div>
-				</div>
-				
+
+				{this.state.showchoosebox ? this.showchoosebox():""}
 
 
 			</div>
