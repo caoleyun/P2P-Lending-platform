6fb3841ef6a1fa24c1c0c134b6a98a657ccee3f4
caoleyun <244814637@qq.com>
2019-05-11 17:55:17 +0800

    P2P-5/11/1环境搭建

--- src/index.js
+++ src/index.js
@@ -1,23 +1,87 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import BECanlendar from './component/BECanlendar/BECanlendar.js';
-import Range from './component/Range/Range.js';
+
+//依赖 。。。  redux  router
+import { Provider } from 'react-redux';
+import { combineReducers , createStore } from 'redux';
+import {BrowserRouter as Router,Route,Link } from 'react-router-dom';
+// import { Route , Link , HashRouter as Router , browserHistory } from 'react-router-dom';
+import { routerReducer } from 'react-router-redux';
+// import createHistory from 'history/createHashHistory';
 
 
-import 'bootstrap/dist/css/bootstrap.css';
+
+//store
+
+//引入组件
+// import BECanlendar from './component/BECanlendar/BECanlendar.js';
+// import Range from './component/Range/Range.js';
+
+//插件
+// import 'bootstrap/dist/css/bootstrap.css';
 
 
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  })
+);
 
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+   
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          rendering
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+    
+  </div>
+)
 
 render(
-	<div>
-		<BECanlendar onpick={ (by,bm,bd,ey,em,ed)=>{} }
-			earliest={{year:new Date().getFullYear(),month:new Date().getMonth()+1,day:new Date().getDate()}}
-			 latest={{year:2019,month:5,day:6}}
-		 />
-		 <Range width={300} min={100} max={7800} />
-	</div>
+	(
+	<Provider store={store} >
+		<Router hashType="hashbang">
+		    <div>
+				<ul>
+					<li><Link to="/">Home</Link></li>
+					<li><Link to="/about">About</Link></li>
+					<li><Link to="/Topics">Topics</Link></li>
+				</ul>
+				<hr/>
+				<Route exact path="/" component={Home}/>
+				<Route path="/about" component={About}/>
+				<Route path="/Topics" component={Topics}/>
+			</div>
+	    </Router>
+	</Provider>
+	)
 ,document.getElementById("root"));
