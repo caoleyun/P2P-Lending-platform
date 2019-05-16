be656d94abf871be0ee3895353ff9df36e4e02c2
caoleyun <244814637@qq.com>
2019-05-16 21:57:01 +0800

    P2P-5/16/3复选框BUG

--- src/component/Databox/SetTableBox.js
+++ src/component/Databox/SetTableBox.js
@@ -5,6 +5,14 @@ import $ from  'jquery/dist/jquery.min.js';
 class SetTableBox extends React.Component{
 	constructor({setSetTableBoxShow,tablecol}){
 		super();
+		this.state=(function(){
+			var o={};
+			tablecol.forEach(function(item){
+				o[item.fieldname]=item.show;
+			});
+			return o;
+		})();
+		console.log(this.state);
 	}
 
 	//组件上树后 调用
@@ -12,12 +20,7 @@ class SetTableBox extends React.Component{
 		$(this.refs.innerbox).css("top","-100%").show().animate({"top":"50%"},500);
 	}
 
-	exit(){
-		let self=this;
-		$(this.refs.innerbox).animate({"top":"-100%"},300,function(){
-			self.props.setSetTableBoxShow(false);
-		});
-	}
+	
 
 	render(){
 		return (
@@ -27,7 +30,7 @@ class SetTableBox extends React.Component{
 					<div className="text-left">
 						{
 							this.props.tablecol.map((item,index)=>{
-								return	<label key={index}><input type="checkbox" readOnly="readonly" checked={item.show} />{item.fieldchinesename}</label>
+								return	<label key={index}><input type="checkbox"  checked={item.show} onChange={(event)=>{this.setc(item.fieldname,event)}} />{item.fieldchinesename}</label>
 							})
 						}
 					</div>
@@ -36,6 +39,23 @@ class SetTableBox extends React.Component{
 			</div>
 		)
 	}
+
+	//点击差号  关闭此组件
+	exit(){
+		let self=this;
+		$(this.refs.innerbox).animate({"top":"-100%"},300,function(){
+			self.props.setSetTableBoxShow(false);
+		});
+	}
+
+	//设置选择的 input
+	setc=(fieldname,event)=>{
+		console.log(fieldname,event.target.checked);
+		console.log(this.state);
+		//改变state
+	}
+
+
 }
 
 export default SetTableBox;
