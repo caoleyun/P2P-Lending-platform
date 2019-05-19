09c2208a180198465507842c71f660f33d2eaf03
caoleyun <244814637@qq.com>
2019-05-19 10:22:29 +0800

    P2P-5/19/2添加后台版

---
+++ build版/www/precache-manifest.927b27852880cbcc5494c123b5e038ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0294a854ba5bb37b9df3cca6796a1f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581d7805d44d8abc209d",
+    "url": "/static/css/2.b78bc511.chunk.css"
+  },
+  {
+    "revision": "3ac38a62b89de0331a2e",
+    "url": "/static/css/main.ecad2a42.chunk.css"
+  },
+  {
+    "revision": "581d7805d44d8abc209d",
+    "url": "/static/js/2.d0c34e84.chunk.js"
+  },
+  {
+    "revision": "3ac38a62b89de0331a2e",
+    "url": "/static/js/main.a7ad12f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
