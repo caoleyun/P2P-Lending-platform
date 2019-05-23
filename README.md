4b047964b54dc2268fc2d157fe4dc65bfed51c85
caoleyun <244814637@qq.com>
2019-05-23 11:42:43 +0800

    初始化上传

--- README.md
+++
@@ -1,2 +0,0 @@
-# P2P-Lending-platform
-P2P借贷平台---个人前端练习项目
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import touzhireducers from './touzhireducers.js';
-
-//列出所有的reducers
-
-export default combineReducers({
-	touzhireducers
-});
